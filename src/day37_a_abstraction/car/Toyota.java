5e9b33005f5271b26c71dd80732b756c10205a42
Pavlo Bychkov <pavlobychkov87@gmail.com>
2025-01-08 22:04:38 -0500

    abstraction and interface intro

---
+++ src/day37_a_abstraction/car/Toyota.java
@@ -0,0 +1,10 @@
+package day37_a_abstraction.car;
+// CONCRETE CLASS
+public class Toyota extends Car {
+
+    @Override
+    public void start() {
+        System.out.println("Toyota is starting by pressing button");
+    }
+
+}

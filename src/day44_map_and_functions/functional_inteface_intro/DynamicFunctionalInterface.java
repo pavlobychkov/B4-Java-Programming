598b33100caf7896ca6f7bef526c6bbcd130a728
Pavlo Bychkov <pavlobychkov87@gmail.com>
2025-01-23 22:04:28 -0500

    Functional interface intro, map recap

---
+++ src/day44_map_and_functions/functional_inteface_intro/DynamicFunctionalInterface.java
@@ -0,0 +1,9 @@
+package day44_map_and_functions.functional_inteface_intro;
+//<E> - Generic - meaning that you can use ANY OBJECT DATA type
+// T is another name that can be sued as well
+// FunctionalInterface - has to have ONLY 1 abstract method.
+
+@FunctionalInterface
+public interface DynamicFunctionalInterface <T> {
+    void test (T t);
+}
